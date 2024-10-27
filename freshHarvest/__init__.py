c930ddcdbc379feaa0773ff1fa4ed35afb563ddc
ke-liang-1160404 <kurt.liang@lincolnuni.ac.nz>
2024-10-28 11:59:51 +1300

    add env file to save database configure information

--- freshHarvest/__init__.py
+++ freshHarvest/__init__.py
@@ -1,13 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
 import pymysql
 pymysql.install_as_MySQLdb()
+#load environment variables
+load_dotenv()
+
+# Database configuration from environment variables
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+
+print("11111",DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+
 
 app= Flask(__name__)
 app.secret_key = 'freshHarvest' 
 #database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:asssks0714@localhost/freshHarvest'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{user}:{password}@{host}/{db_name}'.format(
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    db_name=DB_NAME
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db= SQLAlchemy(app)
 
